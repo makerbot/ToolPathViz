befa1b8da2e9510d2318a664a2bdf0af8dcc9f8c
Ted Brandston <ted@makerbot.com>
2012-07-07 00:42:41 -0400

    fleshing out the visuals.

--- src/model/visual.cpp
+++ src/model/visual.cpp
@@ -1,5 +1,23 @@
 #include "visual.h"
 
-Visual::Visual()
+void Line::renderGL()
 {
+    glColor3i(color.red(), color.green(), color.blue());
+    glBegin(GL_LINES);
+    glVertex3d(from.x(), from.y(), from.z());
+    glVertex3d(to.x(), to.y(), to.z());
+    glEnd();
+}
+
+void Visual::add(const VisualItem *const item)
+{
+    items.append(item);
+}
+
+void Visual::renderGL()
+{
+    foreach(VisualItem* item, items)
+    {
+        item->renderGL();
+    }
 }
--- src/model/visual.h
+++ src/model/visual.h
@@ -2,9 +2,16 @@
 #define VISUAL_H
 
 #include <QColor>
+#include <QList>
 
-#include <step.h>
+#include "step.h"
 
+/*!
+  The base class for displayable shapes.
+
+  If you need to display a shape that is not yet supported, just subclass
+  this and implement renderGL().
+  */
 struct VisualItem
 {
     /** The Step that caused this VisualItem to be created. */
@@ -17,15 +24,43 @@ struct VisualItem
         origin(o),
         color(c)
     {}
+
+    virtual void renderGL() =0;
 };
 
+/*!
+  A simple line.
+  */
+struct Line : public VisualItem
+{
+    const QVector3D from, to;
+
+    Line(const Step& o, const QColor c, const QVector3D f, const QVector3D t) :
+        VisualItem(o, c),
+        from(f),
+        to(t)
+    {}
+
+    void renderGL();
+};
+
+/*!
+    A collection of VisualItems (shapes, lines, etc.) that renders
+    itself.
+
+    If you need to display a shape that is not yet supported, just subclass
+    VisualItem and implement its renderGL() function.
+  */
 class Visual
 {
+private:
+    QList<VisualItem const *const> items;
+
 public:
-    Visual();
+    void add(VisualItem const *const);
 
     /** uses the current openGL context to draw itself */
-    renderGL();
+    void renderGL();
 };
 
 #endif // VISUAL_H
